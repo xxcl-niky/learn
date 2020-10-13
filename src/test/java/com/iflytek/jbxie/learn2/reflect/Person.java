6021cbfe91be73d9dc6982c9cc7c6cccc7eef05f
jbxie <jbxie@iflytek.com>
2020-10-13 17:23:58 +0800

    fix #反射

---
+++ src/test/java/com/iflytek/jbxie/learn2/reflect/Person.java
@@ -0,0 +1,15 @@
+package com.iflytek.jbxie.learn2.reflect;
+
+import lombok.Data;
+
+/**
+ * 人类
+ *
+ * @author jbxie
+ * @create 2020/10/10 15:08
+ */
+@Data
+public class Person {
+    private String firstName;
+    private String lastName;
+}
--- src/test/java/com/iflytek/jbxie/learn2/reflect/ReflectTests.java
+++ src/test/java/com/iflytek/jbxie/learn2/reflect/ReflectTests.java
@@ -7,10 +7,29 @@ package com.iflytek.jbxie.learn2.reflect;
  * @create 2020/09/18 17:27
  */
 
-public class ReflectTests {
+public class ReflectTests{
     public static void main(String[] args) {
-
+        Person person = new Person();
+        person.setFirstName("zhang");
+        person.setLastName("san");
+        System.out.println(getFullName(person));
     }
+
+    public static String getFullName(Person person) {
+        return person.getFirstName() + " " + person.getLastName();
+    }
+
+    /**
+     *
+     * @param object
+     * @return
+     */
+    public static String getFullName(Object object) {
+        int i = 0;
+        Integer J = 0;
+        return null;
+    }
+
 
     public static void classTest() {
     }
